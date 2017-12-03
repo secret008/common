7cd1d91fccaab90359304a57d0e45c244aa81d37
secret qi <secretqi@secretdeMacBook-Pro.local>
2017-12-03 16:35:12 +0800

    first commit

---
+++ app/src/main/java/com/example/secretqi/hellokotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.secretqi.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.initView()
+    }
+
+    private fun initView() {
+        val text1 = findViewById<TextView>(R.id.text1)
+        text1.setText(R.string.str_hello_kotlin)
+    }
+}
